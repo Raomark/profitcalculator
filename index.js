e6904852f69287dc5295ad0a764165ee6d77fbd3
Aekungkun Torpon <114415690+Raomark@users.noreply.github.com>
2024-07-17 10:47:49 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const calBtn = document.getElementById("cal"); // Button Calculator
 const resetBtn = document.getElementById("reset"); // Button Calculator
 
-
 calBtn.addEventListener("click",() => {
 	const numPayment = 3.21/100;
-	const numCom = 5.35/100;
+	const numCom = 7.49/100;
 
 	let disPrice = document.getElementById("price") // แสดงราคา
 	let disFee = document.getElementById("fee") // แสดงค่าชำระเงิน
 	let disCom = document.getElementById("com") // แสดงค่าธรรมเนียม
 	let disResult = document.getElementById("result") // แสดงยอดเงินที่ได้รับ
 	let disprofit = document.getElementById("profit") // แสดงกำไร
+	let empText = document.getElementById("emp-text") // 
 
 	let cost = Number(document.getElementById("cost").value) // ต้นทุน
 	let price = Number(document.getElementById("price").value) // ราคาสินค้า
@@ -23,39 +23,67 @@ calBtn.addEventListener("click",() => {
 	let buyerTotal = priceTotal - spVoucher + shipFeeBuyer;
 	let paymentTransCost = Math.round((buyerTotal + spVoucher) * numPayment);
 	let comFee = Math.round(priceTotal * numCom);
-	let discount = shipFeeShopee - shipFeeBuyer
+	let discount = shipFeeShopee - shipFeeBuyer;
 	let result = priceTotal - paymentTransCost - comFee - discount ;
 	let profit = result - cost - discount - 12 ;
 
-	disPrice.innerHTML = `ราคาสินค้าที่จ่ายรวมส่วนลด = ${priceTotal}`
-	disFee.innerHTML = `ค่าธุรกรรมการชำระเงิน 3.21% = ${paymentTransCost}`
-	disCom.innerHTML = `ค่าธรรมเนียมการขาย 7.48% = ${comFee}`
-	disResult.innerHTML = `รวมยอดเงินที่ผู้ขายได้รับ = ${result}`
-	disprofit.innerHTML = `กำไร(หักค่าส่ง) = ${profit}`
 	
-	if(profit == -12 && result == 0 && comFee == 0 && paymentTransCost == 0 && priceTotal == 0){
-		disprofit.innerHTML = `กำไร(หักค่าส่ง) = 0`
+	if(cost == " " && price == " " && sellerVoucher == " " && spVoucher == " " && shipFeeBuyer == " " && shipFeeShopee == " "){
+		empText.classList.add('alert')
+		empText.classList.add('show')
+		empText.classList.add('open')
+		empText.classList.add('open')
 	}else{
-		disprofit.innerHTML = `กำไร(หักค่าส่ง) = ${profit}`
+		disPrice.innerHTML = `ราคาสินค้าที่จ่ายรวมส่วนลด = ${priceTotal}`;
+		disFee.innerHTML = `ค่าธุรกรรมการชำระเงิน 3.21% = ${paymentTransCost}`;
+		disCom.innerHTML = `ค่ธรรมเนียมการขาย 7.48% = ${comFee}`;
+		disResult.innerHTML = `รวมยอดเงินที่ผู้ขายได้รับ = ${result}`;
+		disprofit.innerHTML = `กำไร(หักค่าส่ง) = ${profit}`;
 	}
 
-	// for(let i = 1; i <=5; i++){
-	// 	console.log(profit * i)
+	// if(profit == -12 && result == 0 && comFee == 0 && paymentTransCost == 0 && priceTotal == 0){
+	// 	disprofit.innerHTML = `กำไร(หักค่าส่ง) = 0`
+	// }else{
+	// 	disprofit.innerHTML = `กำไร(หักค่าส่ง) = ${profit}`
 	// }
 
 })
 
 resetBtn.addEventListener("click", ()=> {
-	let disPrice = document.getElementById("price") // แสดงราคา
-	let disFee = document.getElementById("fee") // แสดงค่าชำระเงิน
-	let disCom = document.getElementById("com") // แสดงค่าธรรมเนียม
-	let disResult = document.getElementById("result") // แสดงยอดเงินที่ได้รับ
-	let disprofit = document.getElementById("profit") // แสดงกำไร
+	let disPrice = document.getElementById("price"); // แสดงราคา
+	let disFee = document.getElementById("fee"); // แสดงค่าชำระเงิน
+	let disCom = document.getElementById("com"); // แสดงค่าธรรมเนียม
+	let disResult = document.getElementById("result"); // แสดงยอดเงินที่ได้รับ
+	let disprofit = document.getElementById("profit"); // แสดงกำไร
+	let empText = document.getElementById("emp-text"); // 
 
 	disPrice.innerHTML = "";
 	disFee.innerHTML = "";
 	disCom.innerHTML = "";
 	disResult.innerHTML = "";
 	disprofit.innerHTML = ""
+	empText.innerHTML = "";
+
 })
 
+fetch("shipWeight.json")
+.then((res) => {
+	return res.json();
+})
+.then((shipWeight) => {
+	let placeholder = document.getElementById("data-output");
+	let out = "";
+	shipWeight.forEach(element => {
+		out += `
+			<tr>
+				<td>${element.weight}</td>
+				<td>${element.bmr}</td>
+				<td>${element.provincial}</td>
+			</tr>
+		`
+	});
+	placeholder.innerHTML = out;
+})
+.catch((error) => {
+	console.log(error.message);
+})
